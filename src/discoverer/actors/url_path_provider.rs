7577d21ee7864ee30a30aca65be221e5fbd66458
Eloy <zer1t0ps@protonmail.com>
2020-04-06 08:15:33 +0200

    rename PathCombinator to UrlPathProvider

---
+++ src/discoverer/actors/url_path_provider.rs
@@ -0,0 +1,36 @@
+use crate::discoverer::communication::{Channel, Receiver};
+use crate::discoverer::http::Url;
+
+pub struct UrlPathProvider {
+    urls: Vec<Url>,
+    paths: Vec<String>,
+    channel: Channel<(Url, Url)>,
+}
+
+impl UrlPathProvider {
+    pub fn new(urls: Vec<Url>, paths: Vec<String>) -> Self {
+        return Self {
+            urls,
+            paths,
+            channel: Channel::new(20),
+        };
+    }
+
+    pub fn combine(self) {
+        for path in self.paths.iter() {
+            for base_url in self.urls.iter() {
+                let url =
+                    base_url.join(&path).expect("error combining path and url");
+                if let Err(error) =
+                    self.channel.sender().send((base_url.clone(), url))
+                {
+                    panic!("Error sending url to aggregator {:?}", error);
+                }
+            }
+        }
+    }
+
+    pub fn receiver(&self) -> &Receiver<(Url, Url)> {
+        return self.channel.receiver();
+    }
+}
