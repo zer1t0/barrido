eb942e53b9ff9b61df5ba2b6e229fe0f34c908a4
Eloy <zer1t0ps@protonmail.com>
2020-04-07 08:51:02 +0200

    chore: adapte HtmlScraperManager to work with ResponseScraper

--- src/discoverer/scraper/scraper_manager.rs
+++ src/discoverer/scraper/scraper_manager.rs
@@ -1,8 +1,6 @@
-use crate::discoverer::http::{Response, ContentType};
-use super::html_scraper::HtmlScraper;
-use super::javascript_scraper::JsScraper;
-use reqwest::Url;
+use crate::discoverer::http::{Response, Url};
 use crate::discoverer::communication::{UrlsMessage, UrlsSender};
+use super::response_scraper::ResponseScraper;
 
 use log::info;
 
@@ -23,39 +21,20 @@ impl ScraperManager for EmptyScraperManager {
 }
 
 pub struct HtmlScraperManager {
-    html_scraper: HtmlScraper,
-    js_scraper: JsScraper,
+    response_scraper: ResponseScraper,
     discovered_paths_sender: UrlsSender,
 }
 
 impl HtmlScraperManager {
     pub fn new(discovered_paths_sender: UrlsSender) -> Self {
         return Self {
-            html_scraper: HtmlScraper::new(),
-            js_scraper: JsScraper::new(),
+            response_scraper: ResponseScraper::new(),
             discovered_paths_sender,
         };
     }
 
-    fn scrap_html(&self, base_url: Url, response: &Response) {
-        let mut urls = self.html_scraper.scrap(response);
-        urls.retain(|url| self.is_a_base_url_subpath(&base_url, &url));
-
-        let urls_message = UrlsMessage::new(base_url, urls);
-
-        self.send_paths(urls_message);
-    }
-
-    fn scrap_javascript(&self, base_url: Url, response: &Response) {
-        let mut urls = self.js_scraper.scrap(response);
-        urls.retain(|url| self.is_a_base_url_subpath(&base_url, &url));
-
-        let urls_message = UrlsMessage::new(base_url, urls);
-        self.send_paths(urls_message);
-    }
-
-    fn send_paths(&self, message: UrlsMessage) {
-        info!("send {} paths", message.urls.len());
+    fn send_urls(&self, message: UrlsMessage) {
+        info!("send {} urls", message.urls.len());
         if message.urls.len() != 0 {
             self.discovered_paths_sender
                 .send(message)
@@ -70,13 +49,9 @@ impl HtmlScraperManager {
 
 impl ScraperManager for HtmlScraperManager {
     fn scrap_response(&self, base_url: Url, response: &Response) {
-        if let Some(content_type) = response.content_type() {
-            match content_type {
-                ContentType::Html => self.scrap_html(base_url, response),
-                ContentType::Javascript => {
-                    self.scrap_javascript(base_url, response)
-                }
-            }
-        }
+        let urls = self.response_scraper.scrap(response);
+        let sub_urls = urls.filter(|u| self.is_a_base_url_subpath(&base_url, u)).collect();
+        let urls_message = UrlsMessage::new(base_url, sub_urls);
+        self.send_urls(urls_message);
     }
 }
